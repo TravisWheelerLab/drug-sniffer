623276ae9125a2ce24fe2ce151429d17e1c27bb0
George Lesica <george@lesica.com>
2022-02-19 11:29:30 -0700

    Use run.sh for stage 6

--- workflow/06-activity_prediction/deps/run.sh
+++ workflow/06-activity_prediction/deps/run.sh
@@ -2,23 +2,33 @@
 
 set -e
 
+# Required parameters
+#
 # The parameters below are required. They are described in the pipeline
 # documentation.
 #
 # LIGAND_NAME
-# PROTEIN_PDB
+# RECEPTOR_PATH
 # DOCKED_PDBQT
+#
+# Full output will end up in OUTPUT_PATH
+#
+# Optional parameters
+#
 # OUTPUT_PATH
+#
+
+OUTPUT_PATH="${OUTPUT_PATH:-ligand.score}"
 
 echo "#pose chemical_name gauss_1 gauss_2 repulsion \
 hydrophobic non_hydrophobic vdw non_dir_hbond_lj \
 non_dir_anti_h_bond_quadratic non_dir_h_bond \
 acceptor_acceptor_quadratic donor_donor_quadratic \
 electrostatic ad4_solvation ligand_length \
-constant_term num_tors_div DFIRE" > "$OUTPUT_PATH"
+constant_term num_tors_div DFIRE" > "${OUTPUT_PATH}_"
 
 # Convert the protein to pdbqt
-obabel -ipdb "$PROTEIN_PDB" -opdbqt -O "${PROTEIN_PDB}qt"
+obabel -ipdb "$RECEPTOR_PATH" -opdbqt -O "${RECEPTOR_PATH}qt"
 
 # Split poses
 vina_split --input "$DOCKED_PDBQT" --ligand pose_
@@ -29,17 +39,20 @@ for pose_pdbqt in pose_*.pdbqt; do
     obabel -ipdbqt "$pose_pdbqt" -omol2 -O "$pose_pdbqt.mol2"
 
     # Capture DFIRE value
-    dfire=$(dligand2-15 -P "$PROTEIN_PDB" -L "$pose_pdbqt" -etype 1)
+    dfire=$(dligand2-15 -P "$RECEPTOR_PATH" -L "$pose_pdbqt.mol2" -etype 1)
 
     # Create all other fields
-    smina=$(smina.static -r "${PROTEIN_PDB}qt" -l "$pose_pdbqt" --score_only \
+    smina=$(smina.static -r "${RECEPTOR_PATH}qt" -l "$pose_pdbqt" --score_only \
         --custom_scoring /opt/activity_prediction/allterms \
             | grep "##" \
             | sed "s/##//" \
             | awk '{if(NR>1) print}')
 
-    echo "$pose_id $LIGAND_NAME $smina $dfire" >> $OUTPUT_PATH
+    echo "$pose_id $LIGAND_NAME $smina $dfire" >> "${OUTPUT_PATH}_"
 
     let "pose_id=pose_id+1"
 done
 
+# Re-score with the ML model
+rescore.py /opt/activity_prediction/platstd.h5 "${OUTPUT_PATH}_" \
+    > "$OUTPUT_PATH"
