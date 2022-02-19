dff9039f9ec23c4c536b4794c165054efc6d7988
George Lesica <george@lesica.com>
2022-02-18 17:11:12 -0700

    Change test dir

--- workflow/04-similarity_search/test-image.sh
+++ workflow/04-similarity_search/test-image.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker run -v $PWD:/data -u $(id -u):$(id -g) \
-    -e DENOVO_LIGANDS_SMI=test/denovo.smi \
-    -e MOLECULE_DB=test/ \
+docker run -v $PWD/test:/data -u $(id -u):$(id -g) \
+    -e DENOVO_LIGANDS_SMI=denovo.smi \
+    -e MOLECULE_DB=. \
     -e TANIMOTO_CUTOFF=0.5 \
     traviswheelerlab/04-similarity_search \
     run.sh
