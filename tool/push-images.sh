6f1654573a1f33582d4fda6eae6126ec63b45574
George Lesica <george@lesica.com>
2022-02-24 16:36:06 -0700

    Add push script

---
+++ tool/push-images.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Convenience script to push all container images.
+
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-docker.io}
+IMAGE_NAMESPACE=${IMAGE_NAMESPACE:-traviswheelerlab}
+IMAGE_VERSION=${IMAGE_VERSION:-latest}
+
+pushd workflow
+
+for stage in 0*; do
+    tag="$IMAGE_REGISTRY/$IMAGE_NAMESPACE/$stage:$IMAGE_VERSION"
+    echo "----------------------------------------"
+    echo "pushing $tag"
+    echo "----------------------------------------"
+    pushd "$stage"
+    docker push "$tag"
+    popd
+done
+
+popd
