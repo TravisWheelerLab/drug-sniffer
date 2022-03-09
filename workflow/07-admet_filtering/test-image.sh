9d05e68a6935571c71fe2f2e4c518378be2f3abb
George Lesica <george@lesica.com>
2022-03-09 15:50:59 -0700

    Fix jlogp install

--- workflow/07-admet_filtering/test-image.sh
+++ workflow/07-admet_filtering/test-image.sh
@@ -8,6 +8,8 @@ docker run -v $PWD/test:/data -u $(id -u):$(id -g) \
     traviswheelerlab/07-admet_filtering \
     runadmet.sh -f test.smi -p 1 -a
 
+# JLogP
+
 docker run -v $PWD/test:/data -u $(id -u):$(id -g) \
     traviswheelerlab/07-admet_filtering \
-    java -jar /opt/admet_filtering/JLogP.jar test.smi jlogp.txt
+    java -jar /opt/JLogP/build/JLogP.jar test.smi jlogp.txt
