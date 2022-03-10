3f37b2d6213961eae0e0b900b668c0330abdcb55
George Lesica <george@lesica.com>
2022-03-10 09:52:19 -0700

    Finish 07 formatting

--- workflow/07-admet_filtering/deps/run.sh
+++ workflow/07-admet_filtering/deps/run.sh
@@ -9,10 +9,20 @@ set -e
 # Optional parameters:
 #
 # ADMET_CHECKS
+# OUTPUT_FILE
 #
+
+OUTPUT_FILE=${OUTPUT_FILE:-output.txt}
 
 for check in ADMET_CHECKS; do
     runadmet.sh -f "$LIGAND_SMI" -p "$check" -a
+    echo -n "${check}_predicted,${check}_confidence,${check}_credibility," >> headers.csv
+    awk 'NR!=1 {printf "%s,%s,%s,",2,3,4}' /opt/fpadmet/RESULTS/predicted.txt >> "_$OUTPUT_FILE"
 done
 
+# TODO: Now append jlogp value(s) to the end of each and then we just cat the two temp files into OUTPUT_FILE
+
 java -jar /opt/admet_filtering/JLogP.jar "$LIGAND_SMI" jlogp.txt
+echo "jlogp" >> headers.csv
+awk '{print $2}' jlogp.txt >> "_$OUTPUT_FILE"
+cat headers.csv "_$OUTPUT_FILE" > "$OUTPUT_FILE"
