e07c2d3c50e95d14627f8f5dc31dd1ab241bef69
George Lesica <george@lesica.com>
2022-03-10 10:34:47 -0700

    Handle multiple smi strings

--- workflow/07-admet_filtering/deps/run.sh
+++ workflow/07-admet_filtering/deps/run.sh
@@ -14,16 +14,25 @@ set -e
 
 OUTPUT_FILE=${OUTPUT_FILE:-output.txt}
 
-rm -f headers.txt "_$OUTPUT_FILE"
+# TODO: Headers should probably be a separate loop?
+rm -f headers.txt
 for check in $ADMET_CHECKS; do
-    runadmet.sh -f "$LIGAND_SMI" -p "$check" -a
-    echo -n "${check}_predicted,${check}_confidence,${check}_credibility," >> headers.csv
-    awk 'NR!=1 {printf "%s,%s,%s,",2,3,4}' /opt/fpadmet/RESULTS/predicted.txt >> "_$OUTPUT_FILE"
+    echo -n "${check}_predicted,${check}_confidence,${check}_credibility," >> headers.txt
+done
+echo "jlogp" >> headers.txt
+
+rm -f data.txt
+cat "$LIGANDS_SMI" | while read smi
+do
+    echo "$smi" > smi.txt
+    for check in $ADMET_CHECKS; do
+        runadmet.sh -f smi.txt -p "$check" -a
+        awk 'NR!=1 {printf "%s,%s,%s,",2,3,4}' /opt/fpadmet/RESULTS/predicted.txt >> data.txt
+    done
+
+    rm -f jlogp.txt
+    java -jar /opt/JLogP/build/JLogP.jar smi.txt jlogp.txt
+    awk '{print $2}' jlogp.txt >> data.txt
 done
 
-rm -f jlogp.txt
-java -jar /opt/JLogP/build/JLogP.jar "$LIGAND_SMI" jlogp.txt
-echo "jlogp" >> headers.csv
-awk '{print $2}' jlogp.txt >> "_$OUTPUT_FILE"
-cat headers.csv "_$OUTPUT_FILE" > "$OUTPUT_FILE"
-
+cat headers.txt data.txt > "$OUTPUT_FILE"
