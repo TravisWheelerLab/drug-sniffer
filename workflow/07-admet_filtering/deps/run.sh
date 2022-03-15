9068d9044104822146af1bc037f5610af726e262
George Lesica <george@lesica.com>
2022-03-15 15:39:37 -0600

    Expand admet script

--- workflow/07-admet_filtering/deps/run.sh
+++ workflow/07-admet_filtering/deps/run.sh
@@ -15,15 +15,18 @@ set -e
 OUTPUT_FILE=${OUTPUT_FILE:-output.txt}
 
 rm -f headers.txt
+echo -n "smi," > headers.txt
 for check in $ADMET_CHECKS; do
     echo -n "${check}_predicted,${check}_confidence,${check}_credibility," >> headers.txt
 done
 echo "jlogp" >> headers.txt
 
 rm -f data.txt
+touch data.txt
 cat "$LIGANDS_SMI" | while read smi
 do
     echo "$smi" > ligand.smi
+    echo -n "$smi," >> data.txt
     for check in $ADMET_CHECKS; do
         runadmet.sh -f ligand.smi -p "$check" -a
         awk 'NR!=1 {printf "%s,%s,%s,",2,3,4}' /opt/fpadmet/RESULTS/predicted.txt >> data.txt
