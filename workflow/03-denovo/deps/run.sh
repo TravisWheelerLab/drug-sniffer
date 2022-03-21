65187c83420e00de07c5bfd19dc85e8bdc22e27c
George Lesica <george@lesica.com>
2022-03-21 10:43:22 -0600

    Update autogrow config

--- workflow/03-denovo/deps/run.sh
+++ workflow/03-denovo/deps/run.sh
@@ -37,31 +37,30 @@ cd /opt/autogrow4 && python RunAutogrow.py \
     --obabel_path "$(which obabel)" \
     --source_compound_file "$SOURCE_COMPOUND_FILE" \
     --root_output_folder "$ORIG_PWD" \
-    --number_of_mutants_first_generation 50 \
-    --number_of_crossovers_first_generation 50 \
     --number_of_mutants 50 \
     --number_of_crossovers 50 \
-    --number_elitism_advance_from_previous_gen 50 \
-    --top_mols_to_seed_next_generation 50 \
-    --diversity_mols_to_seed_first_generation 10 \
-    --diversity_seed_depreciation_per_gen 10 \
-    --docking_executable "/opt/autodock_vina/bin/vina" \
+    --number_elitism_advance_from_previous_gen 70 \
+    --top_mols_to_seed_next_generation 70 \
+    --diversity_mols_to_seed_first_generation 0 \
+    --diversity_seed_depreciation_per_gen 0 \
+    --docking_executable "$(which vina)" \
+    --conversion_choice "ObabelConversion" \
     --num_generations "$NUMBER_OF_GENERATIONS" \
     --number_of_processors "$NUMBER_OF_PROCESSORS" \
     --selector_choice "Rank_Selector" \
-    --max_variants_per_compound 5 \
+    --max_variants_per_compound 1 \
     --filter_source_compounds false \
     --use_docked_source_compounds true \
     --LipinskiLenientFilter \
     --PAINSFilter \
     --docking_exhaustiveness "$DOCKING_EXHAUSTIVENESS" \
+    --start_a_new_run \
     --generate_plot false \
     --rescore_lig_efficiency \
     --scoring_choice "VINA" \
     --dock_choice "VinaDocking" \
     --gypsum_timeout_limit 1 \
-    --rxn_library "all_rxns" \
-    --conversion_choice "ObabelConversion"
+    --rxn_library "all_rxns"
 
 let "GEN0 = $NUMBER_OF_GENERATIONS - 2"
 let "GEN1 = $NUMBER_OF_GENERATIONS - 1"
