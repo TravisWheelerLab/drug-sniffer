8354cff34a207d640132fcfe8038d7dc3ce61f5a
George Lesica <george@lesica.com>
2022-04-07 15:07:35 -0600

    Fix variable name

--- containers/07-admet_prediction/test-image.sh
+++ containers/07-admet_prediction/test-image.sh
@@ -5,7 +5,7 @@ set -e
 # run.sh
 
 docker run -v $PWD/test:/data -u $(id -u):$(id -g) \
-    -e LIGANDS_SMI=test.smi \
+    -e LIGAND_SMI=test.smi \
     -e ADMET_CHECKS="1 2 3" \
     traviswheelerlab/07-admet_prediction \
     run.sh
