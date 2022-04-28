77c72edf46010fd425efcacbdbc4babcf6fda404
George Lesica <george@lesica.com>
2022-04-28 15:27:32 -0600

    Fix stage 6 smi handling

--- containers/06-activity_prediction/test-image.sh
+++ containers/06-activity_prediction/test-image.sh
@@ -12,7 +12,7 @@ rm -f test/autodock_output_pose_*.pdbqt
 # run.sh
 
 docker run -v $PWD/test:/data -u $(id -u):$(id -g) \
-    -e LIGAND_NAME=dummy \
+    -e LIGAND_SMI=test.smi \
     -e RECEPTOR_PDB=protein.pdb \
     -e DOCKED_PDBQT=autodock_output.pdbqt \
     traviswheelerlab/06-activity_prediction:latest \
